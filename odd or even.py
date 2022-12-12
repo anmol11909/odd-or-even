b4b4c38934e49ccdd228f05e28c44328567c79ea
anmol11909 <117017384+anmol11909@users.noreply.github.com>
2022-12-12 20:02:57 +0530

    Add files via upload

---
+++ odd or even.py
@@ -0,0 +1,6 @@
+print("welcome to the rollar coaster")
+height = int(input("what is your height"))
+if height>190:
+  print("Congratulations you can ride")
+else:
+  print("sorry you are not tall enough for this ride, you can go for some other ride")
